17b5b55a126f837c76039b40d7cbaa45c7d7c2c5
UIS Company <appleuiscom.com>
2025-10-08 14:11:39 +0300

    fix search in home screen

--- lib/feature/home/view/widgets/custom_grid_view_for_services.dart
+++ lib/feature/home/view/widgets/custom_grid_view_for_services.dart
@@ -1,7 +1,11 @@
 import 'package:diyar_app/core/extension/padding.dart';
+import 'package:diyar_app/core/style/app_color.dart';
+import 'package:diyar_app/core/style/app_style.dart';
 import 'package:diyar_app/feature/home/controller/home_controller.dart';
 import 'package:diyar_app/feature/home/controller/home_state.dart';
 import 'package:diyar_app/feature/home/view/widgets/grid_view_service_item.dart';
+import 'package:diyar_app/generated/locale_keys.g.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -26,10 +30,31 @@ class CustomGridViewForServices extends StatelessWidget {
       builder: (context, state) {
         final homeController = HomeController.get(context);
         final isLoading = state is GetAllServicesLoadingState;
-        final services = homeController.filteredServices.isNotEmpty
+        final searchText = homeController.searchController.text.trim();
+
+        final services =
+            homeController.filteredServices.isNotEmpty || searchText.isNotEmpty
             ? homeController.filteredServices
             : (homeController.userServicesResponse.data ?? []);
+
+        // لو المستخدم كتب حاجة ومفيش نتائج
+        if (!isLoading && searchText.isNotEmpty && services.isEmpty) {
+          return Padding(
+            padding: EdgeInsets.only(top: 50.h),
+            child: Center(
+              child: Text(
+                LocaleKeys.no_results_found.tr(),
+                style: AppStyle.fontSize16Regular(context).copyWith(
+                  color: AppColors.primaryColor,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          );
+        }
+
         final showSkeleton = isLoading || services.isEmpty;
+
         return Skeletonizer(
           enabled: showSkeleton,
           child: GridView.builder(
