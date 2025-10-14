2f8b7c95a3f3b3a49398769e818333488fd4a4cd
UIS Company <appleuiscom.com>
2025-10-14 11:14:24 +0300

    add bio metric

--- lib/core/functions/app_functions.dart
+++ lib/core/functions/app_functions.dart
@@ -1,8 +1,14 @@
+import 'dart:developer';
 import 'package:cherry_toast/cherry_toast.dart';
 import 'package:cherry_toast/resources/arrays.dart';
 import 'package:diyar_app/core/style/app_color.dart';
+import 'package:diyar_app/core/style/app_style.dart';
+import 'package:diyar_app/generated/locale_keys.g.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:android_intent_plus/android_intent.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AppFunctions {
   static void successMessage(BuildContext context, {required String message}) {
@@ -58,4 +64,95 @@ class AppFunctions {
           : null,
     ).show(context);
   }
+}
+
+void goToDeviceSettings(BuildContext context) async {
+  final Uri iosSettings = Uri.parse('app-settings:');
+  try {
+    if (Theme.of(context).platform == TargetPlatform.android) {
+      final intent = AndroidIntent(
+        action: 'android.settings.SECURITY_SETTINGS',
+      );
+      await intent.launch();
+    } else if (Theme.of(context).platform == TargetPlatform.iOS) {
+      if (!await launchUrl(iosSettings)) {
+        throw 'Could not launch device settings.';
+      }
+    } else {
+      throw 'Unsupported platform.';
+    }
+  } catch (e) {
+    log('Error opening device settings: $e');
+  }
+}
+
+void showLockRequiredDialog(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext dialogContext) {
+      return AlertDialog(
+        title: Text(
+          LocaleKeys.biometric.tr().toUpperCase(),
+          textAlign: TextAlign.center,
+        ),
+        content: Text(
+          LocaleKeys.enableDeviceLockInSettings.tr(),
+          textAlign: TextAlign.center,
+          style: AppStyle.fontSize18Bold(context),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(dialogContext).pop();
+              goToDeviceSettings(context);
+            },
+            child: Text(
+              LocaleKeys.settings.tr(),
+              style: AppStyle.fontSize18Bold(
+                context,
+              ).copyWith(color: AppColors.primaryColor),
+            ),
+          ),
+        ],
+      );
+    },
+  );
+}
+
+Future<bool?> showConfirmationDialog(BuildContext context) async {
+  return showDialog<bool>(
+    context: context,
+    barrierDismissible: false,
+    builder: (BuildContext dialogContext) {
+      return AlertDialog(
+        backgroundColor: AppColors.lightCard,
+        title: Center(child: Text(LocaleKeys.biometric.tr().toUpperCase())),
+        content: Text(
+          LocaleKeys.enableBiometricTextInDialog.tr(),
+          textAlign: TextAlign.center,
+          style: AppStyle.fontSize16Regular(context),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(dialogContext).pop(false);
+            },
+            child: Text(
+              LocaleKeys.cancel.tr(),
+              style: TextStyle(color: AppColors.redColor, fontSize: 15.sp),
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(dialogContext).pop(true);
+            },
+            child: Text(
+              LocaleKeys.setup.tr(),
+              style: TextStyle(color: AppColors.primaryColor, fontSize: 15.sp),
+            ),
+          ),
+        ],
+      );
+    },
+  );
 }
