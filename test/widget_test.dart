f3dd4d42ae8777499da74b3a468c0d9aa830abbe
UIS Company <appleuiscom.com>
2025-11-25 13:14:24 +0200

    try again

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,15 +1,26 @@
-import 'package:diyar_app/core/cubits/language/language_controller.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:diyar_app/feature/internet/controller/internet_controller.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:diyar_app/feature/app/diyar_app.dart';
+import 'package:diyar_app/core/cubits/language/language_controller.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('DiyarApp basic smoke test', (WidgetTester tester) async {
+    final languageController = await LanguageController.create();
+
     await tester.pumpWidget(
-      MultiBlocProvider(providers: [], child: const DiyarApp()),
+      MultiBlocProvider(
+        providers: [
+          BlocProvider<LanguageController>.value(value: languageController),
+          BlocProvider(create: (_) => InternetConnectionController()),
+          // أضف أي Bloc آخر أساسي يحتاجه DiyarApp
+        ],
+        child: const DiyarApp(),
+      ),
     );
 
     await tester.pumpAndSettle();
+
     expect(find.byType(DiyarApp), findsOneWidget);
   });
 }
