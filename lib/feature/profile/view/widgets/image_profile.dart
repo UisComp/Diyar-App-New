31d9ca532b816c6626d538f020af9149474b23b0
Abdo Barakat <johndoe@email.com>
2025-11-26 13:42:35 +0200

    fix profile color

--- lib/feature/profile/view/widgets/image_profile.dart
+++ lib/feature/profile/view/widgets/image_profile.dart
@@ -77,12 +77,15 @@
 //   }
 // }
 
+import 'package:diyar_app/core/cubits/app_theme/app_theme_controller.dart';
+import 'package:diyar_app/core/cubits/app_theme/app_theme_state.dart';
 import 'package:diyar_app/core/style/app_color.dart';
 import 'package:diyar_app/core/style/app_style.dart';
 import 'package:diyar_app/core/widgets/custom_cached_network_image.dart';
 import 'package:diyar_app/feature/profile/model/profile_response_model.dart';
 import 'package:diyar_app/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -93,70 +96,84 @@ class ImageProfile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      padding: EdgeInsets.all(20.r),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(24.r),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black12,
-            blurRadius: 10,
-            offset: Offset(0, 3),
+    return BlocBuilder<AppThemeController, AppThemeState>(
+      builder: (context, state) {
+        final darkTheme =
+            AppThemeController.get(context).currentThemeMode ==
+            AppThemeMode.dark;
+        final cardColor = darkTheme ? AppColors.black87 : AppColors.whiteColor;
+        final cardImageColor = darkTheme
+            ? AppColors.black87
+            : AppColors.secondaryColor;
+        final textColor = darkTheme
+            ? AppColors.containerColor
+            : AppColors.black87;
+        return Container(
+          width: double.infinity,
+          padding: EdgeInsets.all(20.r),
+          decoration: BoxDecoration(
+            color: cardColor,
+            borderRadius: BorderRadius.circular(24.r),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black12,
+                blurRadius: 10,
+                offset: Offset(0, 3),
+              ),
+            ],
           ),
-        ],
-      ),
-      child: Column(
-        children: [
-          /// -----------------------------
-          ///       PROFILE IMAGE
-          /// -----------------------------
-          CircleAvatar(
-            radius: 50.r,
-            backgroundColor: AppColors.containerColor,
-            child: (profile?.profilePicture?.url != null)
-                ? ClipOval(
-                    child: CustomCachedNetworkImage(
-                      imageUrl:
-                          '${profile!.profilePicture!.url!}?v=${DateTime.now().millisecondsSinceEpoch}',
-                      width: 100.r,
-                      height: 100.r,
-                      fit: BoxFit.cover,
-                    ),
-                  )
-                : SvgPicture.asset(
-                    Assets.images.svg.person,
-                    width: 50.w,
-                    height: 50.h,
-                  ),
-          ),
+          child: Column(
+            children: [
+              /// -----------------------------
+              ///       PROFILE IMAGE
+              /// -----------------------------
+              CircleAvatar(
+                radius: 50.r,
+                backgroundColor: cardImageColor,
+                child: (profile?.profilePicture?.url != null)
+                    ? ClipOval(
+                        child: CustomCachedNetworkImage(
+                          imageUrl:
+                              '${profile!.profilePicture!.url!}?v=${DateTime.now().millisecondsSinceEpoch}',
+                          width: 100.r,
+                          height: 100.r,
+                          fit: BoxFit.cover,
+                        ),
+                      )
+                    : SvgPicture.asset(
+                        Assets.images.svg.person,
+                        width: 50.w,
+                        height: 50.h,
+                      ),
+              ),
 
-          SizedBox(height: 10.h),
+              SizedBox(height: 10.h),
 
-          /// -----------------------------
-          ///       USER INFO TEXT
-          /// -----------------------------
-          Text(
-            profile?.name ?? 'Guest',
-            style: AppStyle.fontSize22Bold(context),
+              /// -----------------------------
+              ///       USER INFO TEXT
+              /// -----------------------------
+              Text(
+                profile?.name ?? 'Guest',
+                style: AppStyle.fontSize22Bold(context,).copyWith(color: textColor),
+              ),
+              SizedBox(height: 5.h),
+              Text(
+                profile?.email ?? '',
+                style: AppStyle.fontSize16Regular(
+                  context,
+                ).copyWith(color: AppColors.descContainerColor),
+              ),
+              SizedBox(height: 5.h),
+              Text(
+                profile?.phoneNumber ?? '',
+                style: AppStyle.fontSize16Regular(
+                  context,
+                ).copyWith(color: AppColors.descContainerColor),
+              ),
+            ],
           ),
-          SizedBox(height: 5.h),
-          Text(
-            profile?.email ?? '',
-            style: AppStyle.fontSize16Regular(
-              context,
-            ).copyWith(color: AppColors.descContainerColor),
-          ),
-          SizedBox(height: 5.h),
-          Text(
-            profile?.phoneNumber ?? '',
-            style: AppStyle.fontSize16Regular(
-              context,
-            ).copyWith(color: AppColors.descContainerColor),
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
--- lib/feature/profile/view/widgets/list_view_linked_units.dart
+++ lib/feature/profile/view/widgets/list_view_linked_units.dart
@@ -1,4 +1,7 @@
+import 'package:diyar_app/core/cubits/app_theme/app_theme_controller.dart';
+import 'package:diyar_app/core/cubits/app_theme/app_theme_state.dart';
 import 'package:diyar_app/core/routes/routes_name.dart';
+import 'package:diyar_app/core/style/app_color.dart';
 import 'package:diyar_app/core/widgets/custom_cached.dart';
 import 'package:diyar_app/core/widgets/custom_cached_network_image.dart';
 import 'package:diyar_app/feature/profile/model/unit_model_details_for_linked_user.dart';
@@ -7,6 +10,7 @@ import 'package:diyar_app/feature/profile/view/widgets/empty_linked_units.dart';
 import 'package:diyar_app/generated/locale_keys.g.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:diyar_app/core/extension/sized_box.dart';
 import 'package:go_router/go_router.dart';
@@ -23,85 +27,103 @@ class ListViewLinkedUnits extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      padding: EdgeInsets.all(20.r),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(24.r),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black12,
-            blurRadius: 10,
-            offset: Offset(0, 3),
-          ),
-        ],
-      ),
-      child: Padding(
-        padding: EdgeInsets.all(16.w),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Align(
-              alignment: context.locale.languageCode == 'en'
-                  ? Alignment.centerLeft
-                  : Alignment.centerRight,
-              child: Text(
-                LocaleKeys.own_units.tr(),
-                style: TextStyle(fontSize: 18.sp, fontWeight: FontWeight.bold),
+    return BlocBuilder<AppThemeController, AppThemeState>(
+      builder: (context, state) {
+        final darkTheme =
+            AppThemeController.get(context).currentThemeMode ==
+            AppThemeMode.dark;
+        final cardColor = darkTheme ? AppColors.black87 : AppColors.whiteColor;
+        final cardImageColor = darkTheme
+            ? AppColors.black87
+            : AppColors.secondaryColor;
+        final textColor = darkTheme
+            ? AppColors.containerColor
+            : AppColors.black87;
+        return Container(
+          width: double.infinity,
+          padding: EdgeInsets.all(20.r),
+          decoration: BoxDecoration(
+            color: cardColor,
+            borderRadius: BorderRadius.circular(24.r),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black12,
+                blurRadius: 10,
+                offset: Offset(0, 3),
               ),
-            ),
-            12.ph,
-            linkedUnits.isEmpty
-                ? SizedBox(height: 200.h, child: const EmptyLinkedUnits())
-                : Column(
-                    children: linkedUnits.map((unit) {
-                      return InkWell(
-                        onTap: () async {
-                          context.push(
-                            RoutesName.linkedUnitsDetailScreen,
-                            extra: unit,
-                          );
-                        },
-
-                        child: Padding(
-                          padding: EdgeInsets.only(bottom: 10.h),
-                          child: Row(
-                            children: [
-                                ClipRRect(
-                                  borderRadius: BorderRadius.circular(8.r),
-                                  child: CachedImage(
-                                    url: unit.imageUrl?.url??'',
-                                    width: 50.w,
-                                    height: 50.h,
-                                    fit: BoxFit.cover,
-                                    // errorBuilder:
-                                    //     (context, error, stackTrace) => Icon(
-                                    //       Icons.image_not_supported,
-                                    //       size: 50.w,
-                                    //       color: Colors.grey[400],
-                                    //     ),
-                                  ),
-                                ),
-                              SizedBox(width: 12.w),
-                              Expanded(
-                                child: Text(
-                                  unit.name ?? '',
-                                  style: TextStyle(
-                                    fontSize: 16.sp,
-                                    fontWeight: FontWeight.w500,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      );
-                    }).toList(),
+            ],
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(16.w),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Align(
+                  alignment: context.locale.languageCode == 'en'
+                      ? Alignment.centerLeft
+                      : Alignment.centerRight,
+                  child: Text(
+                    LocaleKeys.own_units.tr(),
+                    style: TextStyle(
+                      fontSize: 18.sp,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-          ],
-        ),
-      ),
+                ),
+                12.ph,
+                linkedUnits.isEmpty
+                    ? SizedBox(height: 200.h, child: const EmptyLinkedUnits())
+                    : Column(
+                        children: linkedUnits.map((unit) {
+                          return InkWell(
+                            onTap: () async {
+                              context.push(
+                                RoutesName.linkedUnitsDetailScreen,
+                                extra: unit,
+                              );
+                            },
+
+                            child: Padding(
+                              padding: EdgeInsets.only(bottom: 10.h),
+                              child: Row(
+                                children: [
+                                  ClipRRect(
+                                    borderRadius: BorderRadius.circular(8.r),
+                                    child: CachedImage(
+                                      url: unit.imageUrl?.url ?? '',
+                                      width: 50.w,
+                                      height: 50.h,
+                                      fit: BoxFit.cover,
+                                      // errorBuilder:
+                                      //     (context, error, stackTrace) => Icon(
+                                      //       Icons.image_not_supported,
+                                      //       size: 50.w,
+                                      //       color: Colors.grey[400],
+                                      //     ),
+                                    ),
+                                  ),
+                                  SizedBox(width: 12.w),
+                                  Expanded(
+                                    child: Text(
+                                      unit.name ?? '',
+                                      style: TextStyle(
+                                        fontSize: 16.sp,
+                                        fontWeight: FontWeight.w500,
+                                        color: textColor,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        }).toList(),
+                      ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
