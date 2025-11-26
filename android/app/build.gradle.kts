4ba376f37c7b5ba1a78cfdd5bd8dca12705b4dfe
UIS Company <appleuiscom.com>
2025-11-26 13:54:40 +0200

    update to google play

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "com.uis.diyar"
         minSdk = flutter.minSdkVersion
         targetSdk = 36
-        versionCode = 8
+        versionCode = 9
         versionName = "1.0.0"
     }
 
