3e4881dde8a9dd78bd4f301c60a0f97aadade361
Eyad Mohamed <88972030+eyadmohamedelzeky@users.noreply.github.com>
2026-04-12 10:48:01 +0200

    feat: configure Android build settings and increment app version code to 11

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "com.uis.diyar"
         minSdk = flutter.minSdkVersion
         targetSdk = 36
-        versionCode = 10
+        versionCode = 11
         versionName = "1.0.0"
     }
 
