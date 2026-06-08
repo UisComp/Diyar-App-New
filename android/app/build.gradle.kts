4fe078525d1326c684b82dbbbc3bbbd399be4d40
Eyad Mohamed <88972030+eyadmohamedelzeky@users.noreply.github.com>
2026-06-08 12:30:38 +0300

    chore: update version code to 16 and version name to 1.0.0+16 for playstore

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "com.uis.diyar"
         minSdk = flutter.minSdkVersion
         targetSdk = 36
-        versionCode = 15
+        versionCode = 16
         versionName = "1.0.0"
     }
 
